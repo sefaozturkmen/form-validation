89b40e8bc602598570f1e7a729e888495fe36c79
sefaozturkmen <sipilett@gmail.com>
2021-02-16 19:39:15 +0300

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { showValidationMessage } from 'src/utils/utils';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,152 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'enoca-sefaozturkmen';
+
+
+  showLoginPassword: string = 'password';
+  loginPasswordToogle: Boolean = false;
+
+  showRegisterPassword: string = 'password';
+  registerPasswordToogle: Boolean = false;
+
+  showConfirmPassword: string = 'password';
+  confirmPasswordToogle: Boolean = false;
+
+  errorsMessage: any = {
+    required: 'Bu alan boş bırakılamaz.',
+    error: 'Geçerli bir değer giriniz.',
+    email: 'Geçerli bir e-posta adresi girmelisiniz.',
+    passwordPattern: 'Şifreniz rakam, büyük, küçük harf ve en az 8 karakter içermelidir.',
+    confirm: 'Şifreler aynı değil.'
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(
+      "^" +
+      "(?=.*[0-9])" +         //at least 1 digit
+      "(?=.*[a-z])" +         //at least 1 lower case letter
+      "(?=.*[A-Z])" +         //at least 1 upper case letter
+      "(?=\\S+$)" +           //no white spaces
+      ".{8,}" +               //at least 4 characters
+      "$"
+    )])
+  })
+
+  get loginEmail(): FormControl {
+    return this.loginForm.controls['email'] as FormControl;
+  }
+
+  get loginPassword() {
+    return this.loginForm.get('password');
+  }
+
+  registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern('^[a-zA-Z]{3,24}$')]],
+    surname: ['', [Validators.required, Validators.pattern('^[a-zA-Z]{3,24}$')]],
+    registerEmail: ['', [Validators.required, Validators.email]],
+    registerPassword: ['', Validators.compose([Validators.required, Validators.pattern(
+      "^" +
+      "(?=.*[0-9])" +         //at least 1 digit
+      "(?=.*[a-z])" +         //at least 1 lower case letter
+      "(?=.*[A-Z])" +         //at least 1 upper case letter
+      "(?=\\S+$)" +           //no white spaces
+      ".{8,}" +               //at least 4 characters
+      "$")])],
+    confirmPassword: ['', Validators.compose([Validators.required])]
+  }, {
+    validator: this.confirmPasswordMatch('registerPassword', 'confirmPassword')
+  });
+
+  confirmPasswordMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmPasswordMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    }
+  }
+
+
+  get registerName() {
+    return this.registerForm.get('name');
+  }
+
+  get registerSurname() {
+    return this.registerForm.get('surname')
+  }
+
+  get registerEmail() {
+    return this.registerForm.get('registerEmail')
+  }
+
+  get registerPassword() {
+    return this.registerForm.get('registerPassword')
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword')
+  }
+
+
+  onChecked() {
+    if (this.loginPasswordToogle) {
+      this.showLoginPassword = 'password';
+      this.loginPasswordToogle = false
+    } else {
+      this.showLoginPassword = 'text';
+      this.loginPasswordToogle = true
+    }
+  }
+
+  onRegisterPasswordChecked() {
+    if (this.registerPasswordToogle) {
+      this.showRegisterPassword = 'password';
+      this.registerPasswordToogle = false
+    } else {
+      this.showRegisterPassword = 'text';
+      this.registerPasswordToogle = true
+    }
+  }
+
+  onConfirmPasswordChecked() {
+    if (this.confirmPasswordToogle) {
+      this.showConfirmPassword = 'password';
+      this.confirmPasswordToogle = false
+    } else {
+      this.showConfirmPassword = 'text';
+      this.confirmPasswordToogle = true
+    }
+  }
+
+  onLoginSubmit() {
+    showValidationMessage(this.loginForm);
+    if (this.loginForm.invalid) return;
+    console.log(this.loginForm.value)
+    this.loginForm.reset();
+  }
+
+  onRegisterSubmit() {
+    showValidationMessage(this.registerForm);
+    if (this.registerForm.invalid) return;
+    console.log(this.registerForm.value)
+    this.registerForm.reset();
+  }
+
+  isAgreeSubmit: Boolean = false;
+
+  agreeSubmit() {
+    if (this.isAgreeSubmit == false) {
+      this.isAgreeSubmit = true;
+    } else {
+      this.isAgreeSubmit = false;
+    }
+  }
 }
+
